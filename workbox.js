b5ab8ffcfe3b18cc2fe8d187ca101d3a87f30ebe
Simon Green <simon@captaincodeman.com>
2019-12-04 14:12:29 -0700

    Add service-worker for PWA score

---
+++ workbox.js
@@ -0,0 +1,64 @@
+const { generateSW } = require('workbox-build');
+const { resolve } = require('path');
+
+const swDest = resolve('docs/sw.js')
+
+console.log('building service worker', swDest);
+
+generateSW({
+  swDest,
+  globDirectory: 'docs',
+  globPatterns: [
+    './index.html',
+    './404.html',
+    './manifest.json',
+    './icons/**/*.png',
+    './scripts/**/*.js',
+    './images/**/*.{png,webp}',
+  ],
+  cacheId: 'rdx-example',
+  skipWaiting: true,
+  clientsClaim: true,
+  navigateFallback: './index.html',
+  runtimeCaching: [{
+    urlPattern: /\/images\//,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'images',
+    },
+  }, {
+    urlPattern: /\/scripts\//,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'images',
+    },
+  },{
+    urlPattern: /^https:\/\/(fonts|storage)\.googleapis\.com\//,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'googleapis',
+    },
+  }, {
+    urlPattern: /^https:\/\/cdn\.jsdelivr\.net\//,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'jsdelivr',
+    },
+  }, {
+    urlPattern: /^https:\/\/unpkg\.com\//,
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'unpkg',
+    },
+  }],
+  offlineGoogleAnalytics: false,
+  cleanupOutdatedCaches: true,
+  navigationPreload: false,
+  sourcemap: true,
+})
+.then(({ count, size }) => {
+  console.log(`Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`);
+})
+.catch((err) => {
+  console.error(`Unable to generate a new service worker.`, err);
+})
