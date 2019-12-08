043556f31ec3611d1377525dc42c3165f203f237
Simon Green <simon@captaincodeman.com>
2019-12-07 22:40:50 -0700

    Add comment about making devtools optional

--- src/store.ts
+++ src/store.ts
@@ -7,6 +7,17 @@ const routingPlugin = routingPluginFactory(routeMatcher)
 
 let store = createStore({ models, plugins: [routingPlugin] })
 
+// These could be commented out if the extra functionality
+// wasn't required, to create a production bundle without
+// the redux devtools enabled for instance. This could be
+// controlled using rollup with the replace plugin, e.g.
+//
+// if (process.env.NODE_ENV !== 'production') {
+//   store = devtools(store)
+// }
+//
+// the bundle size becomes 7.39 Kb minified, 2.81 Kb gzipped
+
 store = persist(store)
 store = devtools(store)
 
