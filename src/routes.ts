4fe3bea644a34a29e243526089489a84470f6960
Simon Green <simon@captaincodeman.com>
2019-12-30 19:18:38 -0700

    Clarify /:app route prefix

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import createMatcher from '@captaincodeman/router'
 
 // NOTE: the /:app prefix of the routes is to handle github pages
+//       using a subfolder and is not required for normal app use
 
 const routes = {
   '/:app/':                  'home-view',
